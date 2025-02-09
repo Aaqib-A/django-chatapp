84e361a7ed0c6e8292962cb8d21d9a390608f8d8
Aaqib-A <aaqib.ansari022@gmail.com>
2025-02-09 22:33:33 +0530

    basic chatapplication done..

--- django_chatapp/rt_chat/views.py
+++ django_chatapp/rt_chat/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from rt_chat.models import ChatGroup
+from rt_chat.forms import ChatMessageCreateForm
 
 # Create your views here.
 
@@ -8,4 +9,22 @@ from rt_chat.models import ChatGroup
 def chat_view(request):
     chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
     chat_messages = chat_group.chat_message.all()[:30]
-    return render(request, 'rt_chat/chat.html', {'chat_messages': chat_messages})
+    form = ChatMessageCreateForm()
+    print(form)
+
+    # if request.request == "POST":
+    if request.htmx:
+        form = ChatMessageCreateForm(request.POST)
+        if form.is_valid:
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            # return redirect('home')
+
+            context = {
+                'message': message,
+                'user': message.author,
+            }
+            return render (request, 'rt_chat/partials/chat_message_p.html', context)
+    return render(request, 'rt_chat/chat.html', {'chat_messages': chat_messages, 'form': form})
