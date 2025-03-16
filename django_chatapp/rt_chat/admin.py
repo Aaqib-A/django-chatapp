1e9fdc12ae5b2ef233909a2e2d181e8698915371
Aaqib-A <aaqib.ansari022@gmail.com>
2025-03-16 17:08:02 +0530

    User online status tracking

--- django_chatapp/rt_chat/admin.py
+++ django_chatapp/rt_chat/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from rt_chat.models import ChatGroup, GroupMessage
 
 # Register your models here.
-admin.site.register(ChatGroup)
+# admin.site.register(ChatGroup)
 admin.site.register(GroupMessage)
+
+
+
+class ChatGroupAdmin(admin.ModelAdmin):
+    filter_horizontal = ('users_online',)  # Enables a UI for selecting users | For debugging purposes
+
+admin.site.register(ChatGroup, ChatGroupAdmin)
--- django_chatapp/rt_chat/consumers.py
+++ django_chatapp/rt_chat/consumers.py
@@ -4,6 +4,8 @@ from django.template.loader import render_to_string
 from asgiref.sync import async_to_sync
 import json
 from rt_chat.models import ChatGroup, GroupMessage
+from users.models import User
+
 
 class ChatroomConsumer(WebsocketConsumer):
     def connect(self):
@@ -60,6 +62,7 @@ class ChatroomConsumer(WebsocketConsumer):
         context = {
             'message': message,
             'user': self.user,
+            'chat_group': self.chatroom,
         }
 
         html = render_to_string("rt_chat/partials/chat_message_p.html", context=context)
@@ -77,12 +80,19 @@ class ChatroomConsumer(WebsocketConsumer):
 
     def online_count_handler(self, event):
         online_count = event['online_count']
+
+        chat_messages = ChatGroup.objects.get(group_name=self.chatroom_name).chat_message.all()[:30]
+        author_ids = set([message.author.id for message in chat_messages])
+        users = User.objects.filter(id__in=author_ids)
+
         context = {
             'online_count': online_count,
             'chat_group': self.chatroom,
+            'users': users
         }
         html = render_to_string("rt_chat/partials/online_count.html", context)
         self.send(text_data=html)
+
 
 class OnlineStatusConsumer(WebsocketConsumer):
     def connect(self):
@@ -121,10 +131,23 @@ class OnlineStatusConsumer(WebsocketConsumer):
 
     def online_status_handler(self, event):
         online_users = self.group.users_online.exclude(id=self.user.id)
+        public_chat_users = ChatGroup.objects.get(group_name='public-chat').users_online.exclude(id=self.user.id)
+        
+        my_chats = self.user.chat_groups.all()
+        private_chats_with_users = [chat for chat in my_chats.filter(is_private=True) if chat.users_online.exclude(id=self.user.id)]
+        group_chats_with_users = [chat for chat in my_chats.filter(groupchat_name__isnull=False) if chat.users_online.exclude(id=self.user.id)]
+
+        if public_chat_users.exists() or private_chats_with_users or group_chats_with_users:
+            online_in_chats = True
+        else:
+            online_in_chats = False
+            
         context = {
             'online_users': online_users,
+            'online_in_chats': online_in_chats,
+            'public_chat_users': public_chat_users,
+            'user': self.user,
         }
 
         html = render_to_string('rt_chat/partials/online_status.html', context=context)
         self.send(text_data=html)
-    
