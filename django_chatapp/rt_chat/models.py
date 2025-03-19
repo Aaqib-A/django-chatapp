899d5c7749c8939e78cc1477d688256fa9d0a181
Aaqib-A <aaqib.ansari022@gmail.com>
2025-03-19 14:10:13 +0530

    upload files

--- django_chatapp/rt_chat/models.py
+++ django_chatapp/rt_chat/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import shortuuid
+from PIL import Image
+import os
 
 # Create your models here.
 class ChatGroup(models.Model):
@@ -17,11 +19,39 @@ class ChatGroup(models.Model):
 class GroupMessage(models.Model):
     group = models.ForeignKey(ChatGroup, related_name='chat_message', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.CharField(max_length=300)
+    body = models.CharField(max_length=300, null=True, blank=True)
+    file = models.FileField(upload_to="files/", blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def filename(self):
+        if self.file:
+            return os.path.basename(self.file.name)
+        else:
+            return None
+        
+    """ 
+    @property
+    def is_image(self):
+        if self.filename.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp')):
+            return True
+        else:
+            return False 
+    """
+    @property
+    def is_image(self):
+        try:
+            image = Image.open(self.file)
+            image.verify()
+            return True
+        except:
+            return False
+
     def __str__(self):
-        return f'{self.author.username}: {self.body}'
+        if self.body:
+            return f'{self.author.username}: {self.body}'
+        elif self.file:
+            return f'{self.author.username}: {self.filename}'
     
     class Meta:
         ordering = ['-created']
--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 
 
-#### [Youtube Tutorial](https://www.youtube.com/watch?v=VSmhitrZ_0w&list=PL5E1F5cTSTtRSP3Qb8-gZ-Hm5AXp3VKvu&index=10)
+#### [Youtube Tutorial](https://www.youtube.com/watch?v=foyhg2Vc5Oc&list=PL5E1F5cTSTtRSP3Qb8-gZ-Hm5AXp3VKvu&index=10)
 
 
-#### [Django Starter](https://github.com/andyjud/django-starter/blob/main/requirements.txt)
+#### [Django Starter](https://github.com/andyjud/django-starter/tree/main)
 
 
 ## Local Setup (Running Django locally)
